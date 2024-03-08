bc9213fd7e287b40dc6a760b9c7ae97531b9fb87
p.maniswaroop <2200030449@kluniversity.in>
2024-03-08 21:32:59 +0530

    footer.js update

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <ul className="menu">
+        <li className="menu__item">
+          <a className="menu__link" href="/Home">
+            Home
+          </a>
+        </li>
+        <li className="menu__item">
+          <a className="menu__link" href="/About">
+            About
+          </a>
+        </li>
+        <li className="menu__item">
+          <a className="menu__link" href="/Contact">
+            Services
+          </a>
+        </li>
+        <li className="menu__item">
+          <a className="menu__link" href="/team">
+            Team
+          </a>
+        </li>
+        <li className="menu__item">
+          <a className="menu__link" href="/contact">
+            Contact
+          </a>
+        </li>
+      </ul>
+      <p>&copy; 2024 AIREASE AIRLINES | All Rights Reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
